df75895151ff9c1dc5d61867b5e99d2a5bca711b
Shigang Li <shigangli.cs@gmail.com>
2020-09-21 14:27:48 +0200

    add div sub mul

--- daceml/onnx/op_implementations/op_implementations.py
+++ daceml/onnx/op_implementations/op_implementations.py
@@ -6,3 +6,83 @@ from dace.libraries.standard.nodes.code import _get_inputs_and_outputs
 from dace.symbolic import symstr
 
 from daceml.onnx.implementation_repository import register_pure_expansion
+
+
+
+@register_pure_expansion("Div")
+def expansion(node, state, sdfg):
+    print("div expanded")
+    inputs, outputs = _get_inputs_and_outputs(sdfg, state, node)
+    node.validate(sdfg, state)
+
+    in_edges = state.in_edges(node)
+    out_edges = state.out_edges(node)
+
+    atype = copy.deepcopy(sdfg.arrays[in_edges[0].data.data])
+    btype = copy.deepcopy(sdfg.arrays[in_edges[1].data.data])
+    ctype = copy.deepcopy(sdfg.arrays[out_edges[0].data.data])
+
+    @dace.program
+    def divop(A: atype, B: btype, C: ctype):
+        C[:] = A / B
+
+    return divop.to_sdfg()
+
+@register_pure_expansion("Add")
+def expansion(node, state, sdfg):
+    print("add expanded")
+    inputs, outputs = _get_inputs_and_outputs(sdfg, state, node)
+    node.validate(sdfg, state)
+
+    in_edges = state.in_edges(node)
+    out_edges = state.out_edges(node)
+
+    atype = copy.deepcopy(sdfg.arrays[in_edges[0].data.data])
+    btype = copy.deepcopy(sdfg.arrays[in_edges[1].data.data])
+    ctype = copy.deepcopy(sdfg.arrays[out_edges[0].data.data])
+
+    @dace.program
+    def addop(A: atype, B: btype, C: ctype):
+        C[:] = A + B
+
+    return addop.to_sdfg()
+
+@register_pure_expansion("Mul")
+def expansion(node, state, sdfg):
+    print("mul expanded")
+    inputs, outputs = _get_inputs_and_outputs(sdfg, state, node)
+    node.validate(sdfg, state)
+
+    in_edges = state.in_edges(node)
+    out_edges = state.out_edges(node)
+
+    atype = copy.deepcopy(sdfg.arrays[in_edges[0].data.data])
+    btype = copy.deepcopy(sdfg.arrays[in_edges[1].data.data])
+    ctype = copy.deepcopy(sdfg.arrays[out_edges[0].data.data])
+
+    @dace.program
+    def mulop(A: atype, B: btype, C: ctype):
+        C[:] = A * B
+
+    return mulop.to_sdfg()
+
+@register_pure_expansion("Sub")
+def expansion(node, state, sdfg):
+    print("sub expanded")
+    inputs, outputs = _get_inputs_and_outputs(sdfg, state, node)
+    node.validate(sdfg, state)
+
+    in_edges = state.in_edges(node)
+    out_edges = state.out_edges(node)
+
+    atype = copy.deepcopy(sdfg.arrays[in_edges[0].data.data])
+    btype = copy.deepcopy(sdfg.arrays[in_edges[1].data.data])
+    ctype = copy.deepcopy(sdfg.arrays[out_edges[0].data.data])
+
+    @dace.program
+    def subop(A: atype, B: btype, C: ctype):
+        C[:] = A - B
+
+    return subop.to_sdfg()
+
+
